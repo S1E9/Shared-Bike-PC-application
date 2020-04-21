df4a03f45ebe58ccc1c3f16002d7feeddbd40389
Eric <36187646+S1E9@users.noreply.github.com>
2020-04-21 10:09:19 +0800

    Update all_ui.py
    Delete my google map API key

--- all_ui.py
+++ all_ui.py
@@ -185,7 +185,7 @@ class Ui_Form(object):
         self.browser = QWebEngineView()
 
         # 加载外部页面，调用
-        # self.browser.load(QtCore.QUrl("https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyA5P1BYw5rP4TGUk0kb4iUevS7-K5d5LQ0"))
+        # self.browser.load(QtCore.QUrl("https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=yourowngooglemapAPIkey"))
         self.browser.setHtml('''
             <!DOCTYPE html>
     <html>
@@ -232,7 +232,7 @@ class Ui_Form(object):
         * The callback parameter executes the initMap() function
         -->
         <script async defer
-        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA5P1BYw5rP4TGUk0kb4iUevS7-K5d5LQ0&callback=initMap">
+        src="https://maps.googleapis.com/maps/api/js?key=yourowngooglemapAPIkey">
         </script>
       </body>
     </html>''')
